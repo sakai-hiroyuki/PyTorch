df7875661abbba7e3499ee63c29bd3b1068f1bc7
酒井裕行 <zyoushu36@gmail.com>
2022-01-13 01:27:02 +0900

    Modified files

--- src/experiments/cifar10.py
+++ src/experiments/cifar10.py
@@ -1,4 +1,3 @@
-from __future__ import annotations
 from typing import Optional
 from time import time
 from tqdm import tqdm
--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-from __future__ import annotations
 import string
 
 from torch import nn
